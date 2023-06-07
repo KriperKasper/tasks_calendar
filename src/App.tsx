06d76898b6f975345c1a6a2cce9880a40d0adb2d
kostiantyn.kashyn <kostiantyn.kashyn@cloudmade.com>
2023-06-07 16:19:43 +0300

    calendar02

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-
+import html2canvas from 'html2canvas';
 import CellContainer from "./components/CellContainer/CellContainer";
 import {ThemeProvider} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 const App: React.FC = () => {
+    const printRef =  React.useRef<HTMLInputElement>(null);
+    const handleDownloadImage = async () => {
+        const element = printRef.current!;
+        const canvas = await html2canvas(element);
+        const data = canvas.toDataURL('image/jpg');
+        const link = document.createElement('a');
+        link.href = data;
+        link.download = 'image.jpg';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
     return (
-            <ThemeProvider>
-                    <CellContainer/>
+            <ThemeProvider >
+                <div ref={printRef}>
+                    <CellContainer  />
+                </div>
+                <Container>
+                    <Row>
+                        <Col sm="12">
+                            <Button variant="primary" onClick={handleDownloadImage}>Download as Image</Button>
+                        </Col>
+                    </Row>
+                </Container>
             </ThemeProvider>
     );
 };
